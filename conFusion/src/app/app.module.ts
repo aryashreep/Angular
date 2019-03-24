35904bd208eae8075ce6fecf4427ba5f324976db
Aryashree Pritikrishna <aryashree.pritikrishna@mindtree.com>
2019-03-24 22:24:50 +0530

    Week 4 : HTTP Part 2 - Angular HTTP Client: Error Handling

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import { baseURL } from './shared/baseurl';
 
@@ -76,6 +77,7 @@ import { baseURL } from './shared/baseurl';
     DishService,
     PromotionService,
     LeaderService,
+    ProcessHTTPMsgService,
     { provide: 'BaseURL', useValue: baseURL }
   ],
   entryComponents: [
